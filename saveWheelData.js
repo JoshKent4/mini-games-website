847c6d9a6421a2f52e84debc3c0e8d01d3c209a8
jkt-repos <34868371+jkt-repos@users.noreply.github.com>
2024-09-17 12:40:16 +0100

    Save Data Automatically Without Clicking A Save Button
    * Remove save button
    * Automatically save when segment or winning lists are updated
    * Fix bug around no winners placeholder after website refresh

--- saveWheelData.js
+++ saveWheelData.js
@@ -1,38 +1,46 @@
-const saveBtn = document.getElementById('save-btn');
-
-function saveWheelData() {
-    const segmentItems = Array.from(segmentList.children).map(item => item.textContent);
-    const winningItems = Array.from(winningList.children).map(item => item.textContent);
-
+function saveSegmentData() {
+    const segmentItems = Array.from(segmentList.children).map(item => item.textContent.slice(0, -1));
     localStorage.setItem('segmentList', JSON.stringify(segmentItems));
-    localStorage.setItem('winningList', JSON.stringify(winningItems));
+    //alert('Wheel data has been saved to your browser\'s local storage.');
+}
 
-    alert('Wheel data has been saved to your browser\'s local storage.');
+function saveWinningData() {
+    const winningItems = Array.from(winningList.children).map(item => item.textContent);
+    localStorage.setItem('winningList', JSON.stringify(winningItems));
 }
 
 function loadWheelData() {
     const savedSegmentList = JSON.parse(localStorage.getItem('segmentList'));
     const savedWinningList = JSON.parse(localStorage.getItem('winningList'));
+    repopulateSegmentList(savedSegmentList);
+    repopulateWinningList(savedWinningList);
+}
 
-    if (savedSegmentList) {
-        segmentList.innerHTML = '';
-        savedSegmentList.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = item;
-            segmentList.appendChild(li);
-        });
+function repopulateSegmentList(savedSegmentList) {
+    savedSegmentList.forEach(savedSegment => {
+        addSavedSegment(savedSegment);
+    });
+}
+
+function addSavedSegment(savedSegment) {
+    if (savedSegment) {
+        segments.push(savedSegment);
+        updateSegmentList();
+        drawWheel();
     }
+}
 
+function repopulateWinningList(savedWinningList) {
     if (savedWinningList) {
         winningList.innerHTML = '';
-        savedWinningList.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = item;
-            winningList.appendChild(li);
+        savedWinningList.forEach(savedName => {
+            addWinningName(savedName);
+
+            if (savedName === 'No-one!') {
+                addWinningListPlaceholder();
+            }
         });
     }
 }
 
-// Event listeners
 document.addEventListener('DOMContentLoaded', loadWheelData);
-saveBtn.addEventListener('click', saveWheelData);
